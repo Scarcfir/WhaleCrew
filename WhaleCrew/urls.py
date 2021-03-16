3e764a40d69319080e8db8196a05c43f496eb2c7
KarolMaciej <k.odrobinski@gmail.com>
2021-03-16 21:50:54 +0100

    Base html

--- WhaleCrew/urls.py
+++ WhaleCrew/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view()),
 ]
