ebb5f315dc1395addfb5a21aa640190e3892530c
KarolMaciej <k.odrobinski@gmail.com>
2021-04-10 11:32:26 +0200

    oborna

--- home/tests/conftest.py
+++ home/tests/conftest.py
@@ -1,16 +1,18 @@
 import pytest
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
+
+from home.models import NewsArticle
 
 
 @pytest.fixture
 def user_data():
-    return {'email': 'emailtest@emailtest.pl', 'name': 'user_name', 'password1': 'user_password', 'password2': 'user_password'}
+    return {'email': 'emailtest@emailtest.pl', 'name': 'user_name', 'password1': 'user_password',
+            'password2': 'user_password'}
 
 
 @pytest.fixture
 def create_test_user(user_data):
-    user_model = User()
-    test_user = user_model.objects.create_user(**user_data)
+    test_user = User.objects.create_user(username="test", password="password", email="email@email.pl")
     return test_user
 
 
@@ -20,3 +22,35 @@ def authenticated_user(client, user_data):
     test_user = user_model.objects.create_user(**user_data)
     client.login(**user_data)
     return test_user
+
+
+@pytest.fixture
+def users():
+    users = []
+    for x in range(1, 11):
+        u = User.objects.create(username=str(x), email=f"email{str(x)}@email.com", password=f"pass{str(x)}")
+        users.append(u)
+    return users
+
+
+@pytest.fixture
+def article_data():
+    return {'title': 'title', 'short_desc': 'desc', 'desc': 'long_desc',
+            'photo_file': ''}
+
+
+@pytest.fixture
+def user_with_permissions():
+    u = User.objects.create(username='aabb')
+    p = Permission.objects.get(codename='home.add_article')
+    u.user_permissions.add(p)
+    return u
+
+
+@pytest.fixture
+def articele():
+    article = []
+    for x in range(1, 11):
+        u = NewsArticle.objects.create(name=str(x))
+        article.append(u)
+    return article
--- home/tests/test_urls.py
+++ home/tests/test_urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django import urls
@@ -27,7 +28,7 @@ def test_check_index(client, param):
 
 
 @pytest.mark.django_db
-def test_user_signup(client, user_data, user):
+def test_user_signup(client, user_data):
     user_model = User
     assert user_model.objects.count() == 0
     signup_url = urls.reverse('Sing_up')
@@ -36,8 +37,13 @@ def test_user_signup(client, user_data, user):
 
 
 @pytest.mark.django_db
-def test_add_article(client, article):
-    assert addarticle.objects.count() == 0
+def test_add_article(client, article_data, users):
+    client.force_login(users[0])
+    response = client.get(reverse('PortfolioView'))
+    assert response.status_code == 200
+    addarticleview = urls.reverse('AddArticle')
+    response = client.post(addarticleview, article_data)
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
@@ -46,3 +52,20 @@ def test_user_logout(client):
     resp = client.get(logout_url)
     assert resp.status_code == 302
     assert resp.url == urls.reverse('NewsList')
+
+
+@pytest.mark.django_db
+def test_user_login(client, create_test_user, user_data):
+    user_model = User.objects.get()
+    data = {'username': user_model.username, 'password': user_model.password, }
+    assert user_model.username == "test"
+    login_url = urls.reverse('Login')
+    resp = client.post(login_url, data)
+    assert resp.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_studio(client, article, user_with_permissions, article_data):
+    client.force_login(user_with_permissions)
+    response = client.post(reverse('AddArticle'), article_data)
+    assert response.status_code == 302
